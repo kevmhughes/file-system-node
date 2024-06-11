c0113c048d6362c7ecbcb09e58b6633aeeaa2eb3
dnlslcd <danielsalcedoq@gmail.com>
2024-06-11 21:16:04 +0200

    Iteración 1 - acabada

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,6 +1,15 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 let dni = "";
 
 /** Leer README */
 
+async function contributorsWithAMistake(){
+    const fileContent = await fs.promises.readFile("./hacienda.json", "utf-8");
+
+    const parsedData = JSON.parse(fileContent);
+    console.log(parsedData);
+}
+
+contributorsWithAMistake();
