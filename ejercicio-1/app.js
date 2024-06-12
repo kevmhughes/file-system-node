ca1adea0990cd956c9712c99dc934d0675818f41
kevmhughes <kevhughes24@hotmail.com>
2024-06-12 04:04:52 +0200

    First commit

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,19 +1,27 @@
-const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
-let dni = "";
+let dni = "98765432C";
 
-/** Leer README */
+const contentHacienda = JSON.parse(fs.readFileSync("./hacienda.json", "utf-8"))
 
-async function contributorsWithAMistake(){
-    const fileContent = await fs.promises.readFile("./hacienda.json", "utf-8");
+let found = false;
+let i = 0;
 
-    const parsedData = JSON.parse(fileContent);
+while(!found && i < contentHacienda.length){
+    found = contentHacienda[i].dni == dni && contentHacienda[i].notificacionesPendientes
+    if (found) {
+        /* console.log(contentHacienda[i].nombre) */
+        const message = `El/la contribuyente ${contentHacienda[i].nombre} tiene notificaciones pendientes. Se enviara un mail a ${contentHacienda[i].email}.\n`
+        /* console.log(message) */
 
-    parsedData.forEach(name => {
-        console.log(name.nombre);
-    });
-    // console.log(parsedData);
+        fs.appendFile("resultado.txt", message, (err) => {
+            if (err) {
+                console.log("Ha ocurrido un error: ", err);
+            } else {
+                console.log("Fichero guardado con éxito.")
+            }
+        })
+    }
+    i++
+
 }
-
-contributorsWithAMistake();
---
+++ ejercicio-2/app.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+const filesArr = [];
+
+// iterates through files folder and pushes file name to filesArr
+const folderPath = "./files";
+fs.readdir(folderPath, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      filesArr.push(file);
+    });
+  }
+
+  console.log(filesArr);
+  for (let i = 0; i < filesArr.length; i++) {
+    // reads file
+    const fileContent = fs.readFileSync(`./files/${filesArr[i]}`, "utf-8");
+
+    // orders content and creates an array
+    const getDataArr = fileContent.split("\n");
+    const getSpecificDataArr = [];
+    const date = filesArr[i].split("_")[0].slice(0,1) + " " + filesArr[i].split("_")[0].slice(1)
+
+    // creates an array of arrays to facilitate access to data
+    getDataArr.forEach((i) => {
+      getSpecificDataArr.push(i.split(","));
+    });
+
+    for (let i = 0; i < getSpecificDataArr.length; i++) {
+      if (
+        // removes teacher and empty array indices
+        getSpecificDataArr[i].length > 1 &&
+        getSpecificDataArr[i][3] == "Sí"
+      ) {
+        // creates message to be sent
+        const message = `${getSpecificDataArr[i][0]} asistió durante un total de ${getSpecificDataArr[i][2]} minutos el ${date}\n`;
+        // appends message to file
+        fs.appendFile("informe.txt", message, (err) => {
+          if (err) {
+            console.log("Ha ocurrido un error: ", err);
+          } else {
+            console.log("Fichero guardado con éxito.");
+          }
+        });
+      }
+    }
+  }
+});
